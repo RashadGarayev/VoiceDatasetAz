bcc4f6f862f79d5e052db6f5a6b86a5bad2ebc7b
Rashad Garayev <pythonaz@yahoo.com>
2024-01-01 22:30:12 +0100

    Update concatcsv.py

--- helper/concatcsv.py
+++ helper/concatcsv.py
@@ -5,7 +5,7 @@ file_path_train = 'train.csv'
 file_path_train_ = 'merge.csv'
 # Load the CSV files into pandas DataFrames
 df_train = pd.read_csv(file_path_train)
-df_train1 = pd.read_csv(file_path_train_)
+df_train_ = pd.read_csv(file_path_train_)
 # Concatenate the two DataFrames vertically (row-wise)
 merged_df = pd.concat([df_train, df_train_], ignore_index=True)
 # Save the merged DataFrame to a new CSV file
