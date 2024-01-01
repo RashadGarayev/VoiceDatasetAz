b71d90f90079555d9d880a037f16c4349ef01376
Rashad Garayev <pythonaz@yahoo.com>
2024-01-01 13:40:02 +0100

    Add files via upload
    merge two csv file

---
+++ helper/concatcsv.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+# Replace 'train.csv' and 'train1.csv' with your actual file paths
+file_path_train = 'train.csv'
+file_path_train_ = 'merge.csv'
+# Load the CSV files into pandas DataFrames
+df_train = pd.read_csv(file_path_train)
+df_train1 = pd.read_csv(file_path_train1)
+# Concatenate the two DataFrames vertically (row-wise)
+merged_df = pd.concat([df_train, df_train_], ignore_index=True)
+# Save the merged DataFrame to a new CSV file
+merged_df.to_csv('merged.csv', index=False)
